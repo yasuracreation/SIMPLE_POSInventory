676cc936212dab44c808017caa612791dc3b12b4
Yasura <yasuracreation@gmail.com>
2020-08-22 15:04:58 +0530

    main structure inventory
    inventory structure updated

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { ProductsComponent } from '../products/products.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FolderPage
+  },
+  {
+    path: 'product',
+    component: ProductsComponent,
+    pathMatch: 'full' 
   }
 ];
 
